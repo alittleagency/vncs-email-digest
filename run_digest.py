d14a5ebc6d1c08badf628504f8bf991e5b81832f
Brittany May <brittanymay@Brittanys-MacBook-Air.local>
2026-05-06 11:24:25 -0400

    Fix import and anthropic version

--- run_digest.py
+++ run_digest.py
@@ -19,7 +19,7 @@ import re
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
-from google.api_python_client import build
+from googleapiclient.discovery import build
 from anthropic import Anthropic
 from bs4 import BeautifulSoup
 from git import Repo
